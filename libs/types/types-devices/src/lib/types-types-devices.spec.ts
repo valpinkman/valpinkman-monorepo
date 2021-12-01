21c9e146909999fb7104e7b5fcbb8029760fb550
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-12-01 16:14:48 +0100

    initial mono repo commit

---
+++ libs/types/types-devices/src/lib/types-types-devices.spec.ts
@@ -0,0 +1,7 @@
+import { typesTypesDevices } from './types-types-devices';
+
+describe('typesTypesDevices', () => {
+  it('should work', () => {
+    expect(typesTypesDevices()).toEqual('types-types-devices');
+  });
+});
